419fa7a97293382aa28b957041afad834b0b5fce
hafeed <hafeedpallikkal@gmail.com>
2023-02-01 12:52:47 +0530

    put added

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,56 @@ class MyApp extends StatelessWidget {
           body: Center(
         child: TextButton(
           onPressed: () {
-            fetchData();
+            // getData();
+            // postData();
+            // getPostData();
+            // putData();
           },
-          child: Text('Post data'),
+          child: Text('data'),
         ),
       )),
     );
   }
 }
 
-void fetchData() async {
+void getData() async {
+  var dio = Dio();
+  var response = await dio.get("https://jsonplaceholder.typicode.com/todos/1");
+  print(response.statusCode);
+  print(response.data.toString());
+}
+
+void postData() async {
+  var dio = Dio();
+  // var response = await dio.get("https://jsonplaceholder.typicode.com/todos/1");
+
+  var response = await dio.post("https://jsonplaceholder.typicode.com/posts",
+      data: {'name': 'hafeed', 'email': 'hafeed@gmail'});
+
+  print(response.statusCode);
+  print(response.data.toString());
+}
+
+void getPostData() async {
   var dio = Dio();
   // var response = await dio.get("");
 
   var response = await Future.wait([
-    dio.get("https://jsonplaceholder.typicode.com/posts/1"),
+    dio.get("https://jsonplaceholder.typicode.com/posts"),
     dio.post("https://jsonplaceholder.typicode.com/posts"),
   ]);
   print(response.length);
   print(response[0].data);
   print(response[1].data);
+}
+
+void putData() async {
+  var dio = Dio();
+  // var response = await dio.get("");
+
+  //!post only create the data , but put is search for the data to update other wise it woll it will create
+  var response = await dio.put("https://jsonplaceholder.typicode.com/posts/1",
+      data: {'name': 'hafeed', 'email': 'hafeedp@gmail.com'});
+
+  print(response.data);
 }
